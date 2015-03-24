a8b86403dd47343eaa562b293bc7d34ba9e6919d
Alexander Morton <alexander.david.morton.cern.ch>
2015-03-24 22:31:10 +0100

    Corrected config file for running Reader through Crab3. Correctly produces output now!

---
+++ test/Crab3_runBTaggingReader_cfg.py
@@ -0,0 +1,94 @@
+import FWCore.ParameterSet.Config as cms
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+###############################
+####### Parameters ############
+###############################
+
+options = VarParsing ('python')
+
+options.register('reportEvery', 1000,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.int,
+    "Report every N events (default is N=10)"
+)
+
+options.register('outputFilename', 'output.root',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "Output file name"
+)
+
+
+options.register('wantSummary', False,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Print out trigger and timing summary"
+)
+
+## 'maxEvents' is already registered by the Framework, changing default value
+options.setDefault('maxEvents', -1)
+
+options.parseArguments()
+
+process = cms.Process("USER")
+
+#process.load("TrackingTools.PatternTools.TSCBLBuilderNoMaterial_cfi")
+#process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+#process.load("Configuration.StandardSequences.MagneticField_cff")
+#process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+#process.load('Configuration.StandardSequences.Reconstruction_cff')
+#process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'PHYS14_25_V1')
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
+
+## Events to process
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+
+## Input files
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        #'/store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/00C90EFC-3074-E411-A845-002590DB9262.root'
+	#MiniAOD - PU20
+	'/store/mc/Phys14DR/TT_Tune4C_13TeV-pythia8-tauola/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/007B37D4-8B70-E411-BC2D-0025905A6066.root'
+	#MiniAOD - PU40
+	#'/store/mc/Phys14DR/TT_Tune4C_13TeV-pythia8-tauola/MINIAODSIM/PU40bx25_tsg_PHYS14_25_V1-v1/00000/06E41ADB-7870-E411-8850-0025905A605E.root'
+    )
+)
+
+## Output file
+process.TFileService = cms.Service("TFileService",
+  fileName = cms.string(options.outputFilename)
+)
+
+## Options and Output Report
+process.options   = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(options.wantSummary),
+    allowUnscheduled = cms.untracked.bool(True)
+)
+
+## Initialize analyzer
+process.bTaggingReader = cms.EDAnalyzer('BTaggingReader',
+
+    outputFilename=cms.untracked.string("placeholderOutputName.root"),
+    jets = cms.InputTag('slimmedJets'), # input jet collection name
+    TriggerEventTag = cms.untracked.InputTag("hltTriggerSummaryAOD", "", "HLT"),
+    PathName = cms.untracked.string("HLT_IsoMu20_eta2p1_IterTrk02_CentralPFJet30_BTagCSV_v1"), #HLT_Mu17_Mu8_v, HLT_IsoMu20_eta2p1_IterTrk02_CentralPFJet30_BTagCSV_v1, HLT_IsoMu24_eta2p1_IterTrk02_CentralPFJet30_BTagCSV_v1, HLT_Ele27_eta2p1_WP85_Gsf_CentralPFJet30_BTagCSV_v1, HLT_Ele32_eta2p1_WP85_Gsf_CentralPFJet30_BTagCSV_v1;
+
+    debug = cms.untracked.bool(False),
+    bDiscriminators = cms.vstring(      # list of b-tag discriminators to access
+        'combinedInclusiveSecondaryVertexV2BJetTags',
+    )
+)
+
+## Let it run
+process.p = cms.Path(process.bTaggingReader)
