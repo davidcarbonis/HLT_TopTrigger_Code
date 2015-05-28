eeaa681475fbd819579561bd3726de8548870170
Alexander <alexander.david.morton@cern.ch>
2015-05-28 13:49:50 +0100

    Updated crab3_configMiniAODReader.py and removed parsing and pset conflicts (which arise when using crab) with runBTaggingReaderMiniAOD_cfg.py.

--- test/crab3_configMiniAODReader.py
+++ test/crab3_configMiniAODReader.py
@@ -3,7 +3,7 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 
-config.General.requestName = 'MC_SingleTop_MiniAOD_PU25_150528'
+config.General.requestName = 'MC_MiniAODTest_150528a'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -14,8 +14,8 @@ config.JobType.psetName = 'runBTaggingReaderMiniAOD_cfg.py'
 config.Data.inputDataset = '/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU40bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM'
 #config.Data.inputDataset = '/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU40bx25_tsg_PHYS14_25_V1-v1/AODSIM'
 
-config.Data.inputDBS = 'global' #Variable to be used when analysing data
-config.Data.useParent = True           # Important!
+config.Data.inputDBS = 'global' 	#Variable to be used when analysing data
+config.Data.useParent = True           	# Important!
 
 config.Data.splitting = 'FileBased' #EventBased for generation, FileBased for analysing data 
 config.Data.unitsPerJob = 100
@@ -24,7 +24,7 @@ config.Data.totalUnits = 100
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.publishDataName = 'CRAB3_SingleTopPaths_MiniAOD_150528'
+config.Data.publishDataName = 'CRAB3_MiniAODTest_150528a'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
 
