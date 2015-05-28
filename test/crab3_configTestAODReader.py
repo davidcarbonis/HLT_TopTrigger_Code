d0d109b96d94604bbc067ca4d6ba6b994e66ab78
Alexander <alexander.david.morton@cern.ch>
2015-05-28 10:23:02 +0100

    Added crab3 scripts to run the Readers for MiniAOD, AOD (AOD cc incomplete and not properly working yet), and the running PAT (AOD->MiniAOD) and then MiniAOD all in one.

---
+++ test/crab3_configTestAODReader.py
@@ -0,0 +1,36 @@
+
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'MC_SingleTop_PU40_25ns'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'runBTaggingReaderTestAOD_cfg.py'
+
+#config.Data.inputDataset = '/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU40bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM'
+config.Data.inputDataset = '/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU40bx25_tsg_PHYS14_25_V1-v1/AODSIM'
+
+config.Data.inputDBS = 'global' #Variable to be used when analysing data
+config.Data.useParent = True           # Important!
+
+config.Data.splitting = 'FileBased' #EventBased for generation, FileBased for analysing data 
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = 1
+
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+
+config.Data.publication = False
+config.Data.publishDataName = 'CRAB3_SingleTopPaths_150520'
+
+config.Site.storageSite = 'T2_UK_London_Brunel'
+
+
+
+
+
+
+
