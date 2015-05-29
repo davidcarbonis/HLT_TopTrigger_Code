cceefd7a85c3f76903131de497fc647816404a4b
Alexander Morton <alexander.david.morton.cern.ch>
2015-05-29 19:48:52 +0200

    Debugging

--- test/crab3_configReRunHLT.py
+++ test/crab3_configReRunHLT.py
@@ -1,25 +1,24 @@
+
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'ReRunHLT_Test_150520a'
+config.General.requestName = 'ReRunHLT_Test_150528'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
-config.General.transferLogs = True
+config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'ReRunHLTandOutput_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
 
 config.Data.inputDataset = '/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU40bx25_tsg_PHYS14_25_V1-v1/GEN-SIM-RAW'
-config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
-
+config.Data.inputDBS = 'global' #Variable to be used when analysing data
+config.Data.splitting = 'FileBased' #EventBased for generation, FileBased for analysing data 
 config.Data.unitsPerJob = 1
-config.Data.totalUnits = 1
+config.Data.totalUnits = 1000
 
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
-config.Data.publishDataName = 'CRAB3_ReRunHLT_150520a'
+config.Data.publishDataName = 'almorton_CRAB3_Test_ReRunHLT_150528'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
-#config.Site.whitelist = ['T2_UK_London_Brunel','T2_US_Florida,T2_CH_CERN','T2_CH_CERN']
-config.Site.blacklist = ['T2_DE_DESY']
