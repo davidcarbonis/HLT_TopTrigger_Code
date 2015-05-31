899bd427c41b97ec82744432f49b20651b3d9f7f
Alexander <alexander.david.morton@cern.ch>
2015-05-31 23:59:17 +0100

    Testing for useParent

--- test/crab3_configTestHltAODReader.py
+++ test/crab3_configTestHltAODReader.py
@@ -3,25 +3,23 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 
-config.General.requestName = 'MC_SingleTop_TestAOD_PU40_150528'
+config.General.requestName = 'MC_SingleTop_TestHltAOD_150531'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'runBTaggingReaderTestAOD_cfg.py'
-
-config.Data.inputDataset = '/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU40bx25_tsg_PHYS14_25_V1-v1/AODSIM'
-config.Data.useParents = '/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU40bx25_tsg_PHYS14_25_V1-v1/AODSIM'
+config.Data.inputDataset = '/TT_Tune4C_13TeV-pythia8-tauola/almorton-almorton_CRAB3_Test_ReRunHLT_150529-834cdd9c15c7858dcaa00386fc68ada7/USER'
 config.Data.inputDBS = 'global' #Variable to be used when analysing data
 config.Data.splitting = 'FileBased' #EventBased for generation, FileBased for analysing data 
-config.Data.unitsPerJob = 100
-#config.Data.totalUnits = 100
+config.Data.unitsPerJob = 10
+config.Data.totalUnits = 10
 
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.publishDataName = 'CRAB3_SingleTopPaths_TestAOD_150528'
+config.Data.publishDataName = 'CRAB3_SingleTopPaths_TestHltAOD_150531'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
 
